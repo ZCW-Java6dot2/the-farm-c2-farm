144c0f1707730a98922c27fb7ef954c0855533ae
gunjan <pandya.gunjan@gmail.com>
2020-11-28 17:05:18 -0500

    push again

--- src/main/java/Farm/ChickenCoop.java
+++ src/main/java/Farm/ChickenCoop.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ChickenCoop {
+    public ChickenCoop() {
+    }
 
     public List<Chicken> getChickenList() {
         return chickenList;
