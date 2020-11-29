70ff49e2d688de0ad6a44adb25b20575254b5610
Earl Atta-Fynn <Earl.Atta.Fynn@gmail.com>
2020-11-28 20:20:12 -0500

    added classes left out in last push

---
+++ src/test/java/Farm/FarmHouseTest.java
@@ -0,0 +1,28 @@
+package Farm;
+
+import Person.Farmer;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FarmHouseTest {
+
+    Farm farm = new Farm();
+    Farmer farmer = new Farmer("Billy Ray");
+    FarmHouse house = farm.getFarmHouse();
+    @Test
+    public void addNewFarmer() {
+    }
+
+    @Test
+    public void removeFarmer() {
+    }
+
+    @Test
+    public void getPersonList() {
+    }
+
+    @Test
+    public void setPersonList() {
+    }
+}
